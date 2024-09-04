8521cfbb4decbde522a111e10999284ae20ff7c2
Ahmad Assaidy <alsaidyxcode@gmail.com>
2024-09-04 12:19:40 +0300

    using name alias for shorter code

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/assaidy/personal-blog-api/handlers"
+	h "github.com/assaidy/personal-blog-api/handlers"
 	"github.com/gorilla/mux"
 )
 
@@ -13,15 +13,15 @@ const Port = ":8080"
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 
-	router.HandleFunc("/posts", handlers.Make(handlers.HandleCreatePost)).
+	router.HandleFunc("/posts", h.Make(h.HandleCreatePost)).
 		Methods("POST")
-	router.HandleFunc("/posts", handlers.Make(handlers.HandleGetAllPosts)).
+	router.HandleFunc("/posts", h.Make(h.HandleGetAllPosts)).
 		Methods("GET")
-	router.HandleFunc("/posts/{id:[0-9]+}", handlers.Make(handlers.HandleGetPostById)).
+	router.HandleFunc("/posts/{id:[0-9]+}", h.Make(h.HandleGetPostById)).
 		Methods("GET")
-	router.HandleFunc("/posts/{id:[0-9]+}", handlers.Make(handlers.HandleUpdatePostById)).
+	router.HandleFunc("/posts/{id:[0-9]+}", h.Make(h.HandleUpdatePostById)).
 		Methods("PUT")
-	router.HandleFunc("/posts/{id:[0-9]+}", handlers.Make(handlers.HandleDeletePostById)).
+	router.HandleFunc("/posts/{id:[0-9]+}", h.Make(h.HandleDeletePostById)).
 		Methods("DELETE")
 	// TODO: add search by term
 
