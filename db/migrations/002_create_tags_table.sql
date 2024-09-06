99fce35b9c307fd7dab12f802fe84ede8b056c76
Ahmad Assaidy <alsaidyxcode@gmail.com>
2024-09-06 15:40:49 +0300

    suggesting a change in tags schema

--- db/migrations/002_create_tags_table.sql
+++ db/migrations/002_create_tags_table.sql
@@ -1,3 +1,11 @@
+-- this is just a piece of shit.
+-- TODO: change the schema to:
+-- CREATE TABLE IF NOT EXISTS tags (
+--     name TEXT NOT NULL,
+--     post_id INTEGER NOT NULL,
+--     PRIMARY KEY (post_id, name),
+--     FOREIGN KEY(post_id) REFERENCES posts(id) ON DELETE CASCADE
+-- );
 CREATE TABLE IF NOT EXISTS tags (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
